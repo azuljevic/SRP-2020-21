55c10d7f1c930a6cf5cb5602d6ab5f511f56dba1
Ana <ana.zuljevic@gmail.com>
2020-11-10 14:42:04 +0100

    start refactoring login

--- postgres-express-node/services/login.service.js
+++ postgres-express-node/services/login.service.js
@@ -1,3 +1,4 @@
+const jwt =require("jsonwebtoken");
 class LoginService {
     constructor({ logger, userModel }) {
       this.userModel = userModel;
@@ -13,7 +14,36 @@ class LoginService {
       });
       return user;
     }
-  
+    async login({username,password}) {
+      const userRecord = await this.userModel.findOne({
+        where: { username },
+      });
+      if (!userRecord) {
+        this.logger.error("User not registered");
+        throw new Error("Authentication failed");
+      }
+
+      if (userRecord.password === password) {
+        this.logger.info("Password correct so proceed and generate a JWT");
+        const user = {
+          username: userRecord.username,
+          role: userRecord.role || "guest",
+        };
+   
+        const payload = {
+          ...user,
+         
+        };
+              const token = this.generateToken(payload);
+              return { user, token }
+      }
+
+    }
+  generateToken(payload) {
+    return jwt.sign(payload, config.jwt.secret, {
+      expiresIn: config.jwt.expiresIn,
+    });
+  }
   }
   
   module.exports = LoginService;
