69cd54daff72b4d9c83d94c0852413ebf1d7c113
Ana <ana.zuljevic@gmail.com>
2020-11-10 13:58:28 +0100

    status

--- postgres-express-node/services/index.js
+++ postgres-express-node/services/index.js
@@ -6,7 +6,11 @@ const { User, MedicalTest } = require("../models");
 const logger = winston.loggers.get("logger");
 
 exports.userServiceInstance = new UserService({ logger, userModel: User });
+
 exports.medicalTestServiceInstance = new MedicalTestService({
   logger,
   testModel: MedicalTest,
 });
+//=======
+//exports.loginServiceInstance = new LoginService({ logger, userModel: User });
+//>>>>>>> 21b237f... inital login logic
